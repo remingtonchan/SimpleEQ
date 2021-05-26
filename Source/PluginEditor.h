475cb6eea888da9b33ae65af532420743aaf04ab
remingtonchan <remingtonchan@gmail.com>
2021-05-27 07:35:06 +0800

    Connected sliders (UI) to the parameters

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -42,6 +42,13 @@ private:
         lowCutFreqSlider, highCutFreqSlider,
 		lowCutSlopeSlider, highCutSlopeSlider;
 
+    using APVTS = juce::AudioProcessorValueTreeState;
+    using Attachment = APVTS::SliderAttachment;
+
+	Attachment peakFreqSliderAttachment, peakGainSliderAttachment, peakQualitySliderAttachment,
+        lowCutFreqSliderAttachment, highCutFreqSliderAttachment,
+        lowCutSlopeSliderAttachment, highCutSlopeSliderAttachment;
+
     std::vector<juce::Component*> getComps();
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (SimpleEQAudioProcessorEditor)
