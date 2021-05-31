50a26a7bcd868a2536b190e154e1c8cc6386dde4
remingtonchan <remingtonchan@gmail.com>
2021-06-01 01:42:14 +0800

    Added frequency labels

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -296,9 +296,9 @@ void ResponseCurveComponent::resized()
 
     Array<float> freqs
     {
-        20, 30, 40, 50, 100,
-        200, 300, 400, 500, 1000,
-        2000, 3000, 4000, 5000, 10000,
+        20, /*30, 40,*/ 50, 100,
+        200, /*300, 400,*/ 500, 1000,
+        2000, /*3000, 4000,*/ 5000, 10000,
     	20000
     };
 
@@ -319,8 +319,6 @@ void ResponseCurveComponent::resized()
     g.setColour(Colours::dimgrey);
 	for (auto x : xs)
 	{
-        //auto normX = mapFromLog10(f, 20.f, 20000.f);
-        //g.drawVerticalLine(getWidth() * normX, 0.f, getHeight());
         g.drawVerticalLine(x, top, bottom);
 	}
 
@@ -334,6 +332,38 @@ void ResponseCurveComponent::resized()
         auto y = jmap(gdB, -24.f, 24.f, static_cast<float>(bottom), static_cast<float>(top));
         g.setColour(gdB == 0.f ? Colour(250, 171, 5) : Colours::darkgreen);
         g.drawHorizontalLine(y, left, right);
+	}
+
+    g.setColour(Colours::lightgrey);
+    const int fontHeight = 10;
+    g.setFont(fontHeight);
+
+	for (int i = 0; i < freqs.size(); ++i)
+	{
+        auto f = freqs[i];
+        auto x = xs[i];
+
+        bool addK = false;
+        String str;
+		if (f > 999.f)
+		{
+            addK = true;
+            f /= 1000.f;
+		}
+
+        str << f;
+        if (addK)
+            str << "k";
+        str << "Hz";
+
+        auto textWidth = g.getCurrentFont().getStringWidth(str);
+
+        Rectangle<int> r;
+        r.setSize(textWidth, fontHeight);
+        r.setCentre(x, 0);
+        r.setY(1);
+
+        g.drawFittedText(str, r, juce::Justification::centred, 1);
 	}
 }
 
